9e8da1cd94f3cde12c3d6c41cb2f368af1638322
Vadim <Vadim@192.168.1.3>
2016-04-21 11:59:38 +0300

    les1zadacha4

---
+++ primer2/src/primer2/zadacha4.java
@@ -0,0 +1,20 @@
+package primer2;
+
+public class zadacha4 {
+	public static void main(String[] args) {
+		double x;
+		double y;
+		
+		x = (double)10;
+		y = (double)56;
+		
+		double a = x + y;
+		double b = x*y;
+		double c = x/y;
+		double d = x-y;
+		
+		System.out.print(a+";"+b+";"+c+";"+d+";");
+		
+		
+	}
+}
