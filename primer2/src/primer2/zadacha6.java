bdabb0f03e2c0d936f2cafaa5445b3b696b2df12
Vadim <Vadim@192.168.1.3>
2016-04-22 21:04:56 +0300

    les1zadacha6

--- primer2/src/primer2/zadacha6.java
+++ primer2/src/primer2/zadacha6.java
@@ -2,18 +2,18 @@ package primer2;
 
 public class zadacha6 {
 	public static void main(String[] args){
-	  int x = -3;
-	  int y = -2;
+	  int x = 0;
+	  int y = 0;
 	
 		
 		
 		if(y>-2&&y<2){
-			if(x>0&&x<4){
+			if(x>=0&&x<4){
 				System.out.println("true");
 			}
 		}
 		else
-			if(y<0&&y>-3){
+			if(y<=0&&y>-3){
 				if(x<4&&x>-4){
 					System.out.println("true");
 				}
